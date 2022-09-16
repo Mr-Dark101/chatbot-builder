07a1786bcfbcf0b5bb57d98c4ff30ac7266d7779
Zubair <urzubairjavaid1@gmail.com>
2022-09-16 11:16:53 +0500

    fixing

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = `http://13.214.26.9:8080`;
-const BASE_URL = `https://637a-111-119-183-11.ngrok.io`;
+const BASE_URL = `https://abeb-111-119-183-14.ngrok.io`;
 
 export const API = axios.create({
     baseURL: BASE_URL,
