1d766ca8e7bd7a6bb0046008a8b635e6dc71c8e0
apple <apple@apples-MacBook-Pro.local>
2022-09-16 09:24:01 +0500

    fixng

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 // const BASE_URL = `http://13.214.26.9:8080`;
-<<<<<<< HEAD
-const BASE_URL = `http://localhost:3030`;
-=======
+
 const BASE_URL = `https://637a-111-119-183-11.ngrok.io`;
->>>>>>> bf68b289d298eb7daa7b60b2c2797f013deaa834
+
 
 export const API = axios.create({
     baseURL: BASE_URL,
