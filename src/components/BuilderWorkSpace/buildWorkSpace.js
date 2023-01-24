c41b62b946be782bcab2a91eb18a3462bba2c68c
apple <ursanir@gmail.com>
2023-01-24 16:11:19 +0500

    fixng

--- src/components/BuilderWorkSpace/buildWorkSpace.js
+++ src/components/BuilderWorkSpace/buildWorkSpace.js
@@ -290,7 +290,7 @@ const BuildWorkSpace = () => {
       dispatch(
          SetCurrentBotUpdateData({
             ...currentBotData,
-            published: !published,
+            published: false,
          })
       );
    };
